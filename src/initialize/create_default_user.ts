02735f56ba69b22a22ad5a8d7b81da0aae1181fc
Kapena Lumumba <217016375@mycput.ac.za>
2024-04-12 14:32:32 +0200

    create default user

--- src/initialize/create_default_user.ts
+++ src/initialize/create_default_user.ts
@@ -1,6 +1,7 @@
 import UserService from "../services/user.service.js";
 
 export default (async function () {
+  console.log("create default user")
   process.env.PETER_KAPENA_PASSWORD &&
     process.env.PETER_KAPENA_EMAIL &&
     (await new UserService().signUp(
--- src/initialize/mongo.ts
+++ src/initialize/mongo.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectToMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("mogo connected")
+    // console.log("mogo connected")
   } catch (error) {
     console.error(error);
     process.exit(1);
