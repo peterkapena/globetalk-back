3dea8dfeb494dc0fe8dddd7f0b0f5e33ad958cdf
Kapena Lumumba <217016375@mycput.ac.za>
2024-04-13 18:52:32 +0200

    process.env.MONGO_URI

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import "reflect-metadata";
 import "./initialize/index.js";
-import  "./initializeApollo.js";
+import "./initializeApollo.js";
 import "./intializeSocket.js";
+
+console.log(process.env.MONGO_URI)
