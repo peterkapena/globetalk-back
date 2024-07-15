4410f498f4692a29b8632da39b3e2fe94241ef8c
Kapena Lumumba <217016375@mycput.ac.za>
2024-07-15 23:10:40 +0200

    remove the nginx binding

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -7,14 +7,25 @@ nginx -g 'daemon off;' &
 sleep 10
 
 # Certificates directory
-CERT_DIR="/etc/letsencrypt"
+CERT_DIR="/etc/letsencrypt/live"
 
-# Check if the certificates already exist and request them if not
-if [ ! -d "$CERT_DIR/live" ]; then
-  certbot --nginx \
-    -n --agree-tos --email peterkapenapeter@gmail.com \
-    --domains api.globetalk.peterkapena.com,io.globetalk.peterkapena.com,globetalk.peterkapena.com
-fi
+# List of domains
+DOMAINS="api.globetalk.peterkapena.com io.globetalk.peterkapena.com globetalk.peterkapena.com"
+
+# Function to check if a certificate exists for a given domain
+check_cert() {
+  DOMAIN=$1
+  if [ ! -d "$CERT_DIR/$DOMAIN" ]; then
+    certbot --nginx \
+      -n --agree-tos --email peterkapenapeter@gmail.com \
+      --domains $DOMAIN
+  fi
+}
+
+# Loop through each domain and request certificates if they don't exist
+for DOMAIN in $DOMAINS; do
+  check_cert $DOMAIN
+done
 
 # Loop to keep the container running and renew certificates
 while :; do
