7a5c183a087739cb4714a5a14fb2785c512cc9e2
Kapena Peter Lumumba <peterkapenapeter@gmail.com>
2024-07-16 23:14:47 +0200

    certbot

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -7,17 +7,22 @@ nginx -g 'daemon off;' &
 sleep 10
 
 # Certificates directory
-CERT_DIR="/etc/letsencrypt"
+CERT_DIR="/etc/letsencrypt/live"
 
 # Check if the certificates already exist and request them if not
-# if [ ! -d "$CERT_DIR/live" ]; then
+if [ ! -d "$CERT_DIR" ]; then
   certbot --nginx \
     -n --agree-tos --email peterkapenapeter@gmail.com \
     --domains api.globetalk.peterkapena.com,io.globetalk.peterkapena.com,globetalk.peterkapena.com,api.mtm.peterkapena.com,mtm.peterkapena.com
-# fi
+else
+  echo "Certificates found. Configuring Nginx with existing certificates..."
+
+  # Deploy hook to reconfigure Nginx
+  certbot renew --deploy-hook "nginx -s reload"
+fi
 
 # Loop to keep the container running and renew certificates
 while :; do
-    certbot renew --quiet
+    certbot renew --quiet --deploy-hook "nginx -s reload"
     sleep 12h
 done
